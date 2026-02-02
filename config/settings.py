d5f1742874cd87c335fe8bc3e6db2544653eb7c1
Fabio <fgadamo@yahoo.co.uk>
2026-02-02 21:33:53 +0100

    Update settings for production (SECRET_KEY from env)

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qheydf33z2j1n4422$ew%(mod*$gk)9x!!6$t9g@*!88u*_zb)'
+import os
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "FabioA00100.eu.pythonanywhere.com",
+    "127.0.0.1",
+    "localhost",
+]
+
 
 
 # Application definition
@@ -121,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
